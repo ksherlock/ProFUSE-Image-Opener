6fd52bf62f4421cf97933b159629c7c0f59d05f4
Kelvin Sherlock <ksherlock@gmail.com>
2011-03-01 22:25:25 -0500

    Initial Commit

---
+++ Image Opener/Image_OpenerAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  Image_OpenerAppDelegate.h
+//  Image Opener
+//
+//  Created by Kelvin Sherlock on 3/1/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Image_OpenerAppDelegate : NSObject <NSApplicationDelegate> {
+@private
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Image Opener/Image_OpenerAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  Image_OpenerAppDelegate.m
+//  Image Opener
+//
+//  Created by Kelvin Sherlock on 3/1/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Image_OpenerAppDelegate.h"
+
+@implementation Image_OpenerAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
